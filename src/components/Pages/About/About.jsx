48d20d576154f99236c33b99f5f1c809339b234a
Leighton Van Ness <leighton@vanness.net>
2026-05-29 13:57:35 -0400

    Tighten about hero and add profile links

--- src/components/Pages/About/About.jsx
+++ src/components/Pages/About/About.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { FaArrowRightLong, FaEnvelope, FaFileLines } from 'react-icons/fa6';
+import {
+  FaArrowRightLong,
+  FaEnvelope,
+  FaFileLines,
+  FaGithub,
+  FaGlobe,
+  FaLinkedinIn,
+} from 'react-icons/fa6';
 import './about.css';
 import headshot from '../../../assets/Headshot.png';
 
@@ -49,6 +56,39 @@ function About() {
           <div>
             <strong>Leighton Van Ness</strong>
             <span>VP, Innovation</span>
+            <div className="hero-card-links">
+              <a
+                className="icon-link"
+                href="https://www.linkedin.com/in/leighton-van-ness-9b901036/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn aria-hidden="true" />
+                <span>LinkedIn</span>
+              </a>
+              <a
+                className="icon-link"
+                href="https://github.com/lvanness7690"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub aria-hidden="true" />
+                <span>GitHub</span>
+              </a>
+              <a
+                className="icon-link"
+                href="https://vanness.net"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGlobe aria-hidden="true" />
+                <span>Portfolio</span>
+              </a>
+              <a className="icon-link" href="mailto:leighton@vanness.net">
+                <FaEnvelope aria-hidden="true" />
+                <span>Email</span>
+              </a>
+            </div>
           </div>
           <dl>
             <div>
