a0cb834522905bc33921ab35c5ee16e5d2b38c96
Leighton Van Ness <leighton@vanness.net>
2026-05-29 11:25:15 -0400

    Remove Whereby SDK from portfolio build

--- src/components/Pages/Meet/Meet.jsx
+++ src/components/Pages/Meet/Meet.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import './meet.css';
-import "@whereby.com/browser-sdk/embed";
-
-// Define a new component that renders the Whereby embed
-const WherebyEmbed = ({ roomUrl }) => {
-    return <whereby-embed room={roomUrl} background="off" style={{ width: '100%' }} />;
-}
 
 function Meet() {
-    // Define your room URL
     const roomUrl = "https://zinc.whereby.com/contact-me2bd65a93-d4dd-41a3-892c-57800d2e0e5d";
 
     return (
@@ -17,10 +10,14 @@ function Meet() {
                 <h2>MEET WITH ME</h2>
                 <p>If you have any questions or just want to say hello, feel free to join me for a quick video meeting:</p>
             </div>
-            
-            {/* Render the Whereby embed below the title and description */}
+
             <div className="whereby-container">
-                <WherebyEmbed roomUrl={roomUrl} />
+                <iframe
+                    src={roomUrl}
+                    title="Whereby meeting room"
+                    allow="camera; microphone; fullscreen; display-capture; autoplay"
+                    style={{ width: '100%', border: 0 }}
+                />
             </div>
         </div>
     );
